676b0e2905f5d48778dc9ae35c9ed1e88a90867c
1amL0st <dyomin.dyominalexander2011@yandex.ru>
2021-03-19 12:11:16 +0300

    Simple websocket connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
-use actix_web::{App, HttpServer};
+use actix::{Actor, StreamHandler};
+use actix_web::{App, HttpRequest, HttpServer, web, HttpResponse};
 use actix_files::{Files};
+use actix_web_actors::ws;
+
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(
+        &mut self,
+        msg: Result<ws::Message, ws::ProtocolError>,
+        ctx: &mut Self::Context,
+    ) {
+        println!("msg = {:?}", msg);
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+async fn start_ws_connection(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, actix_web::Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .service(web::resource("/ws/").to(start_ws_connection))   
             .service(Files::new("/", "./client/").index_file("index.html"))
     })
     .bind("127.0.0.1:8080")?
