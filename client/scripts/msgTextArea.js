e1f74fbe092163be82e49d53e4019bc17863a6ac
1amL0st <dyomin.dyominalexander2011@yandex.ru>
2021-03-24 10:27:18 +0300

    Ignore empty messages

--- client/scripts/msgTextArea.js
+++ client/scripts/msgTextArea.js
@@ -11,9 +11,11 @@ export function MsgTextArea(onSend) {
   document.getElementById('msg-text-area-send-btn').addEventListener('click', onSendMsgBtn.bind(this));
 
   function onSendMsgBtn() {
-    onSend(this.textAreaElement.value);
-    this.textAreaElement.value = '';
-    this.focus();
+    if (this.textAreaElement.value.trim() != '') {
+      onSend(this.textAreaElement.value);
+      this.textAreaElement.value = '';
+      this.focus();
+    }
   }
 
   this.focus = function() {
