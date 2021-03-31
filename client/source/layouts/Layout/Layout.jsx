3129de1cc510d6ec6aeff2e6f4d9cfab2ecce39c
1amL0st <dyomin.dyominalexander2011@yandex.ru>
2021-03-31 13:10:43 +0300

    Fix redirect to sign_up window on every hot reload

--- client/source/layouts/Layout/Layout.jsx
+++ client/source/layouts/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 
+import { useSelector } from 'react-redux';
 import { Route, useHistory } from 'react-router-dom';
 
 import { SignUpWindow } from 'layouts/SignUpWindow';
 import { UserWindow } from 'components/UserWindow';
 import { LeaveWindow } from 'layouts/Header/LeaveWindow';
 import { CreateRoomWindow } from 'components/CreateRoomWindow';
+import { InviteUserWindow } from 'components/InviteUserWindow';
 import { ROUTES } from 'constants/Routes';
 
 import { Header } from 'layouts/Header';
@@ -14,10 +16,17 @@ import { Main } from 'layouts/Main';
 import './Layout.scss';
 
 export const Layout = () => {
+  const isLogged = useSelector((appStore) => appStore.user.isLogged);
   const history = useHistory();
 
   useEffect(() => {
-    history.push('/sign_up');
+    /*
+      NOTE: We need to check if user is logged, because of hot reload rerenders this components
+      This behavior causes go to /sign_up window on every hot reload
+    */
+    if (isLogged) {
+      history.push('/sign_up');
+    }
   }, [history]);
 
   return (
@@ -28,10 +37,21 @@ export const Layout = () => {
           <Main />
         </div>
       </Route>
-      <Route path={ROUTES.SignUp} component={SignUpWindow} />
-      <Route path={ROUTES.UserWindow} component={UserWindow} />
-      <Route path={ROUTES.LeaveWindow} component={LeaveWindow} />
-      <Route path={ROUTES.CreateRoom} component={CreateRoomWindow} />
+      <Route path={ROUTES.SignUp}>
+        <SignUpWindow />
+      </Route>
+      <Route path={ROUTES.UserWindow}>
+        <UserWindow />
+      </Route>
+      <Route path={ROUTES.LeaveWindow}>
+        <LeaveWindow />
+      </Route>
+      <Route path={ROUTES.CreateRoom}>
+        <CreateRoomWindow />
+      </Route>
+      <Route path={ROUTES.InviteUser}>
+        <InviteUserWindow />
+      </Route>
     </div>
   );
 };
